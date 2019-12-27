6bd1b231d91f6a13ca47189f6266f6cc56f39473
Sergey Zelentsov <_sharapov_@mail.ru>
2019-12-27 13:30:07 +0300

    Домашняя работа 3 обе задачи

--- src/homework3.java
+++ src/homework3.java
@@ -12,7 +12,37 @@ public class homework3 {
       – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
     */
     public static void main(String[] args) throws IOException{
-        gameOne();
+
+        int ansver;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+
+            System.out.println("Выберите иру 1 - Игра1 / 2 - Игра2 / 0 - если нехотите играть");
+            try {
+                ansver = Integer.parseInt(reader.readLine());
+                if (ansver == 1) {
+                    System.out.println("Игра1 началась");
+                    gameOne();
+
+                } else if (ansver == 2) {
+                    System.out.println("Игра2 началась");
+                    gameToo();
+
+
+                }else if (ansver == 0) {
+
+                    System.out.println("Вы отказались играть");
+                    System.out.println("Программа завершена");
+                    break;
+                }
+                else
+                {System.out.println();}
+            } catch (NumberFormatException e) {
+                System.out.println("Вводить необходимо только цифры 1 или 2");
+            }
+        }
+        reader.close();
+
     }
     public static void gameOne()throws IOException{
         Random random = new Random();
@@ -28,6 +58,7 @@ public class homework3 {
                     ansver = Integer.parseInt(reader.readLine());
                     if (ansver == a) {
                         System.out.println("Правильно это число " + a);
+                        break;
                     } else if (ansver < a) {
                         System.out.println("Загаданное число больше " + ansver);
                     } else if (ansver > a) {
@@ -44,10 +75,11 @@ public class homework3 {
                     ansver = Integer.parseInt(reader.readLine());
                     if (ansver == 1) {
                         bool = true;
+                        a = random.nextInt(10);
                         break;
                     } else if (ansver == 0) {
                         bool = false;
-                        System.out.println("Программа завершается");
+                        System.out.println("Игра завершается");
                         break;
                     }
                     else System.out.println("«Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет)");
@@ -56,21 +88,14 @@ public class homework3 {
                 }
             }
         }
-        reader.close();
     }
 
-        public static void gameToo()
+        public static void gameToo ()throws IOException
         {
-            //String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-
-
-            /*
-            * Создать массив из слов
+             /*Создать массив из слов
     String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"}.
-            При запуске программы компьютер загадывает слово,
-             запрашивает ответ у пользователя, сравнивает его с загаданным словом и сообщает,
-             правильно ли ответил пользователь. Если слово не угадано, компьютер показывает буквы,
-             которые стоят на своих местах.
+    При запуске программы компьютер загадывает слово,запрашивает ответ у пользователя, сравнивает его с загаданным словом и сообщает,
+    правильно ли ответил пользователь. Если слово не угадано, компьютер показывает буквы,которые стоят на своих местах.
     apple – загаданное
     apricot - ответ игрока
     ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
@@ -79,6 +104,41 @@ public class homework3 {
     char a = str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции
     Играем до тех пор, пока игрок не отгадает слово.
     Используем только маленькие буквы.*/
+            String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String answer;
+            String[] request = {"#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"};
+            Random random = new Random();
+            int a = random.nextInt(words.length);
+            String quest = words[a];
+            //System.out.println(quest);
+            System.out.println("Загадано слово отгадайте его");
+            boolean bool = true;
+            while (bool){
+                for(String reques :request){
+                    System.out.print(reques);
+                }
+                System.out.println();
+                answer = reader.readLine();
+                if (answer.equals(quest)){
+                    System.out.printf("Ты выиграл %s првильное слово", answer);
+                    bool = false;
+                    System.out.println();
+                    System.out.println("Игра завершена");
+                }
+                else {
+                    try {
+                        System.out.println(answer + " неверный ответ");
+                        for (int i = 0; i < answer.length(); i++) {
+                            if (answer.charAt(i) == quest.charAt(i)) {
+                                request[i] = Character.toString(answer.charAt(i));
+                            }
+                        }
+                    }
+                    catch (StringIndexOutOfBoundsException e){
+                    }
+                }
+            }
         }
 
 

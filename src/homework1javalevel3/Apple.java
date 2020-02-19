3b74856b99a9c8d0750c7d25dd706710b1087f31
Sergey Zelentsov <_sharapov_@mail.ru>
2020-02-19 22:01:03 +0300

    дзджава3

---
+++ src/homework1javalevel3/Apple.java
@@ -0,0 +1,10 @@
+package homework1javalevel3;
+
+public class Apple extends Fruit{
+    private float weight = 1.0f;
+
+    @Override
+    public float getWeight1() {
+        return weight;
+    }
+}
---
+++ src/homework1javalevel3/Fruit.java
@@ -0,0 +1,8 @@
+package homework1javalevel3;
+
+public abstract class Fruit {
+    float weight;
+    float getWeight1() {
+        return weight;
+    }
+}
